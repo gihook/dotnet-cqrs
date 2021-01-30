f7b145189a441f064826ee75b6d573c496356ad8
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2021-01-30 05:24:30 +0100

    feat: implemented automatic registration of actions

--- DomainModules/AuctionsModule/Commands/CreateAuction.cs
+++ DomainModules/AuctionsModule/Commands/CreateAuction.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using Action.Interfaces;
+using Action.Models;
+
 namespace AuctionModule.Commands
 {
-    public class CreateAuction
+    public class CreateAuction : Command<int>
     {
+        public override IEnumerable<ValidationError> Validate(Executor executor)
+        {
+            return Enumerable.Empty<ValidationError>();
+        }
 
+        protected override int ExecuteInternal(Executor executor)
+        {
+            return 1;
+        }
     }
 }
--- Util/InjectionConfig/ConfigureInjection.cs
+++ Util/InjectionConfig/ConfigureInjection.cs
@@ -1,13 +1,37 @@
 ﻿using Autofac;
 using Action.Core;
+using Action.Interfaces;
+using System.Linq;
+using AuctionModule.Commands;
 
 namespace InjectionConfig
 {
-    public class ConfigureInjection
+    public static class ConfigureInjection
     {
         public static void Configure(ContainerBuilder builder)
         {
-	     RegisterInjection.RegisterActionCore(builder);
+            RegisterInjection.RegisterActionCore(builder);
+
+            // NOTE: use ANY class from domain module
+            // this is the only way to automatically register
+            // Actions
+            // It is not possible to reference DLL without
+            // using at least one class in module
+            RegisterAllActionsFromModule<CreateAuction>(builder);
+        }
+
+        private static void RegisterAllActionsFromModule<T>(ContainerBuilder builder)
+        {
+            var types = typeof(T).Assembly.GetTypes();
+            var names = types.Select(t => t.Name);
+            var actions = types;
+
+            foreach (var action in actions)
+            {
+                var name = action.Name;
+                System.Console.WriteLine("action: " + name);
+                builder.RegisterType(action).Keyed(name, typeof(IAction));
+            }
         }
     }
 }
